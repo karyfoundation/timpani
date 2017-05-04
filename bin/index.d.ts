38351b84e7ed1cd095718974ecfea969216e76f8
Pouya Kary ☂️ <k@karyfoundation.org>
2017-05-04 12:24:13 +0430

    Delete index.d.ts

--- bin/index.d.ts
+++
@@ -1,6 +0,0 @@
-export declare type TTokenType = 'string' | 'bold' | 'italic';
-export interface IToken {
-    type: TTokenType;
-    value: IToken[] | string;
-}
-export declare function parse(inlineMarkdown: string): IToken[];
